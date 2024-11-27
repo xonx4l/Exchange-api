dc7b61300a780f74efd3e5533f055efec41bf90e
xonx4l <sk797119@gmail.com>
2024-11-27 09:37:12 +0530

    api-server

---
+++ src/orderbook.ts
@@ -0,0 +1,28 @@
+interface Order {
+    price: number;
+    quantity: number;
+    orderid: string;
+}
+
+interface Bid extends Order {
+    side: 'bid',
+}
+
+interface Ask extends Order {
+    side: 'ask',
+}
+
+interface Orderbook {
+    bids: Bid[];
+    asks: Ask[];
+}
+
+export const orderbook: Orderbook = {
+    bids: [],
+    asks: [],
+}
+
+export const bookWithQuantity: {bids: {[price:number]:number}; asks:{[price:number]:number}} ={
+    bids:{},
+    asks:{},
+}
